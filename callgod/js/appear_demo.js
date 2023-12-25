73a9825ed876d087083cfedfde83d1550a555dda
ru3tjp6 <148740162+ru3tjp6@users.noreply.github.com>
2023-12-25 14:45:41 +0800

    CallGod

---
+++ callgod/js/appear_demo.js
@@ -0,0 +1,5 @@
+var tag = document.getElementById("rp_demo");
+var button = document.querySelector("button");
+button.addEventListener("click", function () {
+  tag.classList.toggle("rp_demo");
+});
